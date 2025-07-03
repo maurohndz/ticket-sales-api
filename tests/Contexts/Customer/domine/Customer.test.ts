c332ff858ec8c5b1d4463f819edad4abc96eb73c
Mauro Hernandez <mauroDeveloper@hotmail.com>
2025-07-02 22:07:30 -0400

    Refactor TypeScript configuration to improve test directory inclusion and enhance Jest support; update package.json to streamline testing scripts and remove deprecated entries.

---
+++ tests/Contexts/Customer/domine/Customer.test.ts
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Customer } from "../../../../src/Contexts/BoxOffice/Customer/domain/Customer";
+
+describe('[DOMINE] - Customer', () => {
+    /**
+     * Crear usuario con los datos requeridos
+     */
+    it('Crear usuario con los datos requeridos', () => {
+        const customer = new Customer({
+            names: 'John',
+            lastName: 'Doe',
+            email: 'john.doe@example.com',
+            status: true,
+        });
+
+        expect(customer).toBeDefined();
+        expect(customer.id).toBeDefined();
+        expect(customer.names).toBe('John');
+        expect(customer.lastName).toBe('Doe');
+        expect(customer.email).toBe('john.doe@example.com');
+    });
+
+    it('Crear usuario con los todos los datos', () => {
+        const customer = new Customer({
+            id: uuidv4(),
+            names: 'John',
+            lastName: 'Doe',
+            email: 'john.doe@example.com',
+            status: true,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            deletedAt: new Date(),
+        });
+
+        expect(customer).toBeDefined();
+        expect(customer.id).toBeDefined();
+        expect(customer.names).toBe('John');
+        expect(customer.lastName).toBe('Doe');
+        expect(customer.email).toBe('john.doe@example.com');
+        expect(customer.status).toBe(true);
+        expect(customer.createdAt).toBeDefined();
+        expect(customer.updatedAt).toBeDefined();
+        expect(customer.deletedAt).toBeDefined();
+    });
+});
